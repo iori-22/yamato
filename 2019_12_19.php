e6c7f5baae42f5f8246f49ea20eb6918384375e8
iori22 <59114861+iori-22@users.noreply.github.com>
2019-12-23 00:59:14 +0900

    Add files via upload

---
+++ 2019_12_19.php
@@ -0,0 +1,14 @@
+<?php
+// ifとelseifを使って、変数が1ならなかじ、2ならワードプレス、ABCならヒトデ
+$hoge = 1;
+
+if( $hoge == 1){
+  echo 'なかじ';
+}elseif( $hoge == 2){
+  echo 'ワードプレス';
+}elseif( $hoge == ABC){
+  echo 'ヒトデ';
+}
+
+
+?>
